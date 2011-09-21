6cbc7c8fada73618d1622edc9dab5d194e0f6d6b
Sean Dunn <sean@attamusc.com>
2011-09-21 10:43:18 -0400

    Will this ever work

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ function start(route, handle) {
 
   }
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(process.env.PORT | 8001);
   console.log("Server has started.");
 }
 
